839f243922ea7c81d51a70c0edb7b6b0e9008a89
bulutluoz <abbulutluoz@gmail.com>
2023-06-21 12:39:24 +0200

    7.gun sonu

---
+++ src/test/java/tests/day07_testBase_dropdown/C05_BasicAuthentication.java
@@ -0,0 +1,28 @@
+package tests.day07_testBase_dropdown;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class C05_BasicAuthentication extends TestBase {
+
+    @Test
+    public void test01(){
+        //2- https://the-internet.herokuapp.com/basic_auth sayfasina gidin
+        //3- asagidaki yontem ve test datalarini kullanarak authentication’i yapin
+        //
+        //Html komutu : https://username:password@URL
+        //	Username     : admin
+        // 	password      : admin
+        //
+
+        driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+        //4- Basarili sekilde sayfaya girildigini dogrulayin
+
+        WebElement basariliGirisYazisi = driver.findElement(By.tagName("p"));
+        Assert.assertTrue(basariliGirisYazisi.isDisplayed());
+        bekle(3);
+    }
+}
