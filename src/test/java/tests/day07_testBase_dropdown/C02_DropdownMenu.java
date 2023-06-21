d461edf393dac73ebb50bdf85de7eb6a58806a47
bulutluoz <abbulutluoz@gmail.com>
2023-06-21 09:47:33 +0200

    7.gun sonu

---
+++ src/test/java/tests/day07_testBase_dropdown/C02_DropdownMenu.java
@@ -0,0 +1,26 @@
+package tests.day07_testBase_dropdown;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class C02_DropdownMenu extends TestBase {
+
+    @Test
+    public void test01(){
+
+        // Amazon anasayfaya gidin
+
+        // arama kutusunun yanindaki dropdown menude 34 secenek oldugunu test edin
+
+        // dropdown menuden Baby kategorisini secin
+
+        // Arama kutusuna Nutella yazip aratin
+
+        // bulunan sonuc sayisinin 10'dan fazla oldugunu test edin
+
+        // sectigimiz opsiyonu yazdirin
+
+        // sectigimiz opsiyon'un Baby oldugunu test edin
+    }
+
+}
