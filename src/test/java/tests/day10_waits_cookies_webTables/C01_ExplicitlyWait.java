10f0ebdd26fdc331f4333f90cb3fe45b3511f17c
bulutluoz <abbulutluoz@gmail.com>
2023-07-04 09:56:43 +0200

    10.gun sonu

---
+++ src/test/java/tests/day10_waits_cookies_webTables/C01_ExplicitlyWait.java
@@ -0,0 +1,81 @@
+package tests.day10_waits_cookies_webTables;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class C01_ExplicitlyWait {
+
+
+    //2. Iki tane metod olusturun : implicitWait() , explicitWait()
+    //
+    //	 Iki metod icin de asagidaki adimlari test edin.
+    //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+    //4. Remove butonuna basin.
+    //5. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+    //6. Add buttonuna basin
+    //7. It’s back mesajinin gorundugunu test edin
+
+
+    @Test
+    public void implicitlyWaitTest(){
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        //4. Remove butonuna basin.
+        driver.findElement(By.xpath("//*[text()='Remove']")).click();
+        //5. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+        WebElement itsGoneElementi = driver.findElement(By.xpath("//*[text()=\"It's gone!\"]"));
+        Assert.assertTrue(itsGoneElementi.isDisplayed());
+
+        //6. Add buttonuna basin
+        driver.findElement(By.xpath("//*[text()=\"Add\"]")).click();
+        //7. It’s back mesajinin gorundugunu test edin
+        WebElement itsBackElementi = driver.findElement(By.xpath("//*[text()=\"It's back!\"]"));
+        Assert.assertTrue(itsBackElementi.isDisplayed());
+        driver.close();
+    }
+
+    @Test
+    public void explicitlyWaitTesti(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        //3. https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/dynamic_controls");
+        //4. Remove butonuna basin.
+        driver.findElement(By.xpath("//*[text()='Remove']")).click();
+        //5. “It’s gone!” mesajinin goruntulendigini dogrulayin.
+
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(40));
+        /*
+            visibilityOf() method'u bize 2 secenek sunuyor
+            - Eger webelement'i locate edebiliyorsak, once locate edip
+              sonra o elementi istenen condition icin beklemesini isteyebiliriz
+            - Eger kullanacagimiz elementi locate EDEMIYORSAK
+              o zaman locate islemi ile bekleme islemini birlikte yapabiliriz.
+         */
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()=\"It's gone!\"]")));
+        WebElement itsGoneElementi = driver.findElement(By.xpath("//*[text()=\"It's gone!\"]"));
+        Assert.assertTrue(itsGoneElementi.isDisplayed());
+        //6. Add buttonuna basin
+        
+        //7. It’s back mesajinin gorundugunu test edin
+        
+        
+    }
+}
